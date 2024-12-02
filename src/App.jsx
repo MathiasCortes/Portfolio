6f07b81042f8541cb868d070d93c413bcafff129
AgustinChamorro-dev <agustinmarksell@gmail.com>
2024-12-02 01:10:14 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error from "./pages/Error.jsx";
 import Home from "./pages/Home.jsx";
 import Edit from "./pages/Edit.jsx";
-import Login from "./pages/Login.jsx";
 import "./styles/App.css";
 
 const routes = createBrowserRouter([
   { path: "/", element: <Home /> },
-  { path: "/Inicio", element: <Login /> },
   { path: "/Editar", element: <Edit /> },
   { path: "/Error", element: <Error /> },
 ]);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,48 @@
 import React, { useState } from "react";
-
+import "../styles/App.css" 
 const Home = () => {
-  const [isHidden, setIsHidden] = useState("Hidden");
-  const [sideMenuIcon, setSideMenuIcon] = useState(
-    "../assets/svg/bars-solid.svg"
-  );
+  const [isHidden, setIsHidden] = useState(false);
+  const [sideMenuIcon, setSideMenuIcon] = useState(false);
 
-  const handleClickShow = () => {
+ /*  const handleClickShow = () => {
     setIsHidden(isHidden === "Hidden" ? "Shown" : "Hidden");
     setSideMenuIcon(
       isHidden === "Hidden"
         ? "../assets/svg/xmark-solid.svg"
         : "../assets/svg/bars-solid.svg"
     );
-  };
+  }; */
+
+  
+    const icon = isHidden ? ( <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"  className="ToggleIcon" 
+      
+      alt="Toggle Menu">
+        {/* Icono cuando está visible */}
+        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+      </svg>
+     
+    ) : (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="ToggleIcon">
+        {/* Icono cuando está oculto */}
+        <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+
+      </svg>
+    );
+  
+    const  handleClick = () => {
+      setIsHidden(!isHidden)
+    }
+  
+  
+
 
   return (
     <div>
       <nav className="navbar">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          width="40"
-          height="40"
-          fill="currentColor"
-          alt="Toggle Menu"
-          onClick={handleClickShow}
-          style={{ cursor: "pointer" }}
-        >
-          <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-        </svg>
+        <button  onClick={handleClick}>
+          {icon}
+        </button>
+        
       </nav>
       <aside className={`sideBar ${isHidden}`}>
         <></>
