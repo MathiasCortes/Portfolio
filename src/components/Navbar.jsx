e114e09ba4efba3893a55604de5e6c8b6a86a5f6
AgustinChamorro-dev <agustinmarksell@gmail.com>
2024-12-04 14:23:36 -0600

    tailwind
    css a tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,77 @@
 import React, { useState } from "react";
-import "../styles/Navbar.css" 
-
-
+import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [sideMenuIcon, setSideMenuIcon] = useState(false);
 
-    const [isHidden, setIsHidden] = useState(false);
-    const [sideMenuIcon, setSideMenuIcon] = useState(false); 
+  const toggleList = sideMenuIcon ? (
+    <ul className="absolute pl-8 m-0 text-white list-none bg-gray-800 rounded-md top-14 right-1">
+      <li className= "toggleMenu-item">
+        <a href="#" className="navbarToggle-linkItems">Ajustes</a>
+      </li>
+      <li className= "toggleMenu-item">
+        <a href="#" className="navbarToggle-linkItems">b</a>
+      </li>
+      <li className= "toggleMenu-item">
+        <a href="#" className="navbarToggle-linkItems">c</a>
+      </li>
+      <li className= "toggleMenu-item">
+        <a href="#" className="navbarToggle-linkItems">d</a>
+      </li>
+    </ul>
+  ) : null;
 
-    
-const toggleList = sideMenuIcon ? ( 
-<ul className="navbar-menu">
-        <li><a href="#">Ajustes</a></li>
-        <li><a href="#">b</a></li>
-        <li><a href="#">c</a></li>
-        <li><a href="#">d</a></li>
-</ul>
-) : (null) 
-
-
-const icon = isHidden ? ( <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"  className="ToggleIcon" 
-    alt="Toggle Menu">
+  const icon = isHidden ? (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"
+      className="ToggleIcon"
+      alt="Toggle Menu"
+    >
       {/* Icono cuando está visible */}
       <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
     </svg>
-   
   ) : (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="ToggleIcon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+      className="ToggleIcon"
+    >
       {/* Icono cuando está oculto */}
       <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-
     </svg>
   );
 
-  const  handleClick = () => {
-    setIsHidden(!isHidden)
-    setSideMenuIcon(!sideMenuIcon)
-  }
+  const handleClick = () => {
+    setIsHidden(!isHidden);
+    setSideMenuIcon(!sideMenuIcon);
+  };
   return (
-    <nav className="navbar">
-      <div className="navbar-brand"><img src="" alt="Logo" /></div>
-      <ul className="navbar-links">
-        <li><a href="#">a</a></li>
-        <li><a href="#">b</a></li>
-        <li><a href="#">c</a></li>
-        <li><a href="#">d</a></li>
+    <nav className="flex items-center px-2 py-2 text-white bg-gray-800 place-content-between">
+      <button className="text-2xl font-bold bg-neutral-950">
+        {/* <img src="" alt="Logo" /> */}asaaaaaa
+      </button>
+      <ul className="flex gap-4 list-none">
+        <li className="nav-item">
+          <a href="#">a</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">b</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">c</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">d</a>
+        </li>
       </ul>
-       <button className="ButtonToggleIcon" onClick={handleClick}>
-          {icon}
-        </button>
-        
-        {toggleList}
+      <button className="bg-white border rounded-none" onClick={handleClick}>
+        {icon}
+      </button>
+
+      {toggleList}
     </nav>
-    
   );
 };
 
