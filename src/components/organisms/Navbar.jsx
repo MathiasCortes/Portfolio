bc1ac4dbfffba25ee74c801bb11fe423148d964e
Mathias Nahuel Cortes Fabra <arthourgrimm@gmail.com>
2024-12-12 22:25:40 -0300

    ReArrange

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Logo from "../../assets/svg/LogoStar.png";
-import "../../styles/animation.css"
+
 const Navbar = () => {
   const [isHidden, setIsHidden] = useState(false);
   const [sideMenuIcon, setSideMenuIcon] = useState(false);
@@ -9,70 +9,59 @@ const Navbar = () => {
     setSideMenuIcon(!sideMenuIcon);
   };
   const toggleList = sideMenuIcon ? (
-    <div
-        className ={`${isHidden ? 'activeToggle' : 'hiddenToggle'}`}
-
-      >
-    <ul className="relative transition duration-500">
-      {" "}
-      {/* -z-10 */}
-      <li className="toggleMenu-item">
-        <a className=" navToggle-listItems-a translate-" href="#Ajustes">
-          Opcion 1
-        </a>
-      </li>
-      <li className="toggleMenu-item">
-        <a className="navToggle-listItems-a" href="#Home">
-          Opcion 2
-        </a>
-      </li>
-      <li className="toggleMenu-item">
-        <a className="navToggle-listItems-a" href="#increible">
-          Opcion 3
-        </a>
-      </li>
-      <li className="toggleMenu-item">
-        <a className="navToggle-listItems-a" href="#maravilloso">
-          Opcion 4
-        </a>
-      </li>
-      <li className="toggleMenu-item">
-        <a className="navToggle-listItems-a" href="#maravilloso">
-          Opcion 5
-        </a>
-      </li>
-    </ul>
+    <div className={`${isHidden ? "activeToggle" : "hiddenToggle"}`}>
+      <ul className="relative transition duration-500">
+        {" "}
+        {/* -z-10 */}
+        <li className="toggleMenu-item">
+          <a className=" navToggle-listItems-a translate-" href="#Ajustes">
+            Opcion 1
+          </a>
+        </li>
+        <li className="toggleMenu-item">
+          <a className="navToggle-listItems-a" href="#Home">
+            Opcion 2
+          </a>
+        </li>
+        <li className="toggleMenu-item">
+          <a className="navToggle-listItems-a" href="#increible">
+            Opcion 3
+          </a>
+        </li>
+        <li className="toggleMenu-item">
+          <a className="navToggle-listItems-a" href="#maravilloso">
+            Opcion 4
+          </a>
+        </li>
+        <li className="toggleMenu-item">
+          <a className="navToggle-listItems-a" href="#maravilloso">
+            Opcion 5
+          </a>
+        </li>
+      </ul>
     </div>
   ) : null;
 
-  
-
   const iconToggle = isHidden ? (
-    
-    <button className="relative flex items-center justify-center w-12 h-12 overflow-hidden transition duration-500 transform bg-white border rounded-sm hover:scale-95"
-    onClick={handleClick}>
-     
-    <span className="absolute w-10 h-1 transition-transform duration-300 -rotate-45 bg-black rounded-sm">
-    </span>
-    <span className="absolute w-10 h-1 transition-transform duration-300 rotate-45 bg-black rounded-sm">
-    </span>
-    <span className="absolute w-10 h-1 transition-transform duration-300 translate-x-12 bg-black rounded-sm">
-    </span>
+    <button
+      className="relative flex items-center justify-center w-12 h-12 overflow-hidden transition duration-500 transform bg-white border rounded-sm hover:scale-95"
+      onClick={handleClick}
+    >
+      <span className="absolute w-10 h-1 transition-transform duration-300 -rotate-45 bg-black rounded-sm"></span>
+      <span className="absolute w-10 h-1 transition-transform duration-300 rotate-45 bg-black rounded-sm"></span>
+      <span className="absolute w-10 h-1 transition-transform duration-300 translate-x-12 bg-black rounded-sm"></span>
     </button>
   ) : (
-  
-    <button className="relative flex items-center justify-center w-12 h-12 overflow-hidden transition duration-300 transform bg-white border rounded-sm hover:scale-95"
-    onClick={handleClick}>
-        <span className="transition-transform duration-300 -translate-y-3 lineToggle">
-    </span>
-    <span className="transition-transform duration-300 translate-y-3 lineToggle">
-    </span>
-    <span className="transition-transform duration-300 -translate-x-0 lineToggle">
-    </span>
+    <button
+      className="relative flex items-center justify-center w-12 h-12 overflow-hidden transition duration-300 transform bg-white border rounded-sm hover:scale-95"
+      onClick={handleClick}
+    >
+      <span className="transition-transform duration-300 -translate-y-3 lineToggle"></span>
+      <span className="transition-transform duration-300 translate-y-3 lineToggle"></span>
+      <span className="transition-transform duration-300 -translate-x-0 lineToggle"></span>
     </button>
   );
 
-  
   return (
     <nav className="relative z-10 flex items-center px-2 py-2 text-white bg-gray-800 border-b-2 border-gray-950 place-content-between">
       <img src={Logo} alt="Logo" className="h-10 w-100" />
@@ -99,11 +88,10 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
-     
-        {iconToggle}
-        
+
+      {iconToggle}
+
       {toggleList}
-      
     </nav>
   );
 };
